c00da8e373294cabf6dde2ef28e0f19eade73749
Mike <wrzosekm71@gmail.com>
2020-02-13 23:10:41 +0000

    Adding Sign in,Sign Up, Form and Custom Button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+//CSS
 import './App.css';
 
+//COMPONENTS
+import Header from './components/header/header.component';
+
+//PAGES
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
+import SignInSignOutPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
+
+
 
 function App() {
   return (
@@ -14,6 +21,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignOutPage} />
       </Switch>      
     </div>
   )
